1897f20be926a382b4a7571c1f298a95e8c40ef5
Kshitij Lahoti <78255433+Kshtij-7@users.noreply.github.com>
2021-05-04 14:47:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+var playerX = 10, playerY = 10;
+var blockH = 30, blockW = 30;
+
+var fab = new fabric.Canvas("myCanvas")
+
+var playerOBJ = "", blockOBJ = "";
+
+function addPlayer() {
+    fabric.Image.fromURL('player.png', function (Img) {
+        playerOBJ = Img;
+        playerOBJ.scaleToWidth(150)
+        playerOBJ.scaleToHeight(140)
+        playerOBJ.set({
+            top: playerY,
+            left: playerX
+        });
+        canvas.add(playerOBJ)
+    })
+}
+function addBlock(getBlock) {
+    fabric.Image.fromURL(getBlock, function (Img) {
+        blockOBJ = Img;
+        blockOBJ.scaleToWidth(blockW)
+        blockOBJ.scaleToHeight(blockH)
+        blockOBJ.set({
+            top: playerY,
+            left: playerX
+        });
+        canvas.add(blockOBJ)
+    })
+}
+
+canvas.addEventlistener('keypress', mykeypress)
+
+function mykeypress(e) {
+    keypressed = e.keyCode;
+    if (keypressed == '37') {
+        left();
+    }
+    if (keypressed == '38') {
+        up();
+    }
+    if (keypressed == '39') {
+        right();
+    }
+    if (keypressed == '40') {
+        down();
+    }
+}
